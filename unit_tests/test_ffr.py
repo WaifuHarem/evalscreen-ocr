7a9248b6158037e6e54a5895c1b245e7e95c278b
abraker <abraker95@gmail.com>
2020-08-01 15:14:56 -0400

    Move FFR screenshots to a dedicated directory

--- unit_tests/test_ffr.py
+++ unit_tests/test_ffr.py
@@ -7,7 +7,7 @@ from ffr.ffr_core import FfrCore
 class TestFFR(unittest.TestCase):
 
     def test_ffr(self):
-        filename = 'unit_tests/data/ffr/test1.png'
+        filename = 'unit_tests/data/ffr/ss/test1.png'
         _, data = FfrCore(filename).process()
 
         print(data)
